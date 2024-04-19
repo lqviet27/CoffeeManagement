926d4e934c616fb54531db702aca1bbdab487c80
lqviet27 <lequocviet2752004@gmail.com>
2024-04-19 18:56:09 +0700

    Model: add model

---
+++ Model/BillDetail.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class BillDetail
+    {
+        private int _id;
+        private int _billId;
+        private int _drinkId;
+        private int _quantity;
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public int billId
+        {
+            get { return _billId; }
+            set { _billId = value; }
+        }
+        public int drinkId
+        {
+            get { return _drinkId; }
+            set { _drinkId = value; }
+        }
+        public int quantity
+        {
+            get { return _quantity; }
+            set { _quantity = value; }
+        }
+
+        public BillDetail() { }
+        public BillDetail(int id, int billId, int drinkId, int quantity)
+        {
+            this._id = id;
+            this._billId = billId;
+            this._drinkId = drinkId;
+            this._quantity = quantity;
+        }
+    }
+}
---
+++ Model/DrinkType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class DrinkType
+    {
+        private int _id;
+        private string _name;
+
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public DrinkType() { }
+        public DrinkType(int id, string name)
+        {
+            this._id = id;
+            this._name = name;
+        }
+    }
+}
